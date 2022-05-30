9f394d4f3980dd52824eb2f62cc7019f64b5449e
ShiKunLun <82928229+ShiKunLin@users.noreply.github.com>
2022-05-30 15:10:41 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -19,11 +19,16 @@ line_bot_api.push_message('U02fc785bca1c72493e65e003e46b8db0', TextSendMessage(t
 
 #訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
+import re
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-   message =event.message.text
-   line_bot_api.reply_message(event.reply_token,TextSendMessage(message))
-
+    message = event.message.text
+    if re.match("你是誰",message):
+        line_bot_api.reply_message(event.reply_token,TextSendMessage("才不告訴你勒~~"))
+    else:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(message))
+    
 import os 
 #主程式
 if __name__ == "__main__":
